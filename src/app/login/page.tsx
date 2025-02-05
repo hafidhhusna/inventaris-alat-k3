88cd5c0efcf26b2c02a55e04f0b9cc8f3fec6e0c
hafidhhusna <hfdhusna@gmail.com>
2025-02-05 08:45:47 +0700

    modified : login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,8 @@
 import { LoginPage } from "@/components/ui/auth/LoginPage";
-import { SignUpForm } from "@/components/ui/register/auth/SignUpForm";
+// import { SignUpForm } from "@/components/ui/register/auth/SignUpForm";
 
 export default function Login(){
     return(
-        <SignUpForm />
+        <LoginPage />
     )
 }
