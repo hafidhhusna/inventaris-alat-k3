99e0e6290a9d430f45dcd456c723f2ab77461637
hafidhhusna <hfdhusna@gmail.com>
2025-02-11 23:48:05 +0700

    removing some redundant features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,11 @@ const Header = () => {
   return (
     <div className="w-full h-[6.198vw] bg-[#FFFFFF] text-[#000] flex items-center justify-evenly">
       <div className="flex">
-        <form action="">
-          <input
-            type="text"
-            className="w-[30.417vw] h-[2.656vw] rounded-[2.5vw] bg-[#F4F4F4] px-[1vw] mr-[1vw]"
-          />
-        </form>
-        <button className="rounded-full w-[2.656vw] h-[2.656vw] bg-[#f4f4f4]"></button>
       </div>
       <div className="flex items-center justify-center">
         <button className="rounded-full w-[2.656vw] h-[2.656vw] bg-[#51B5DD] mr-[1vw]"></button>
         <Link
-          href="/ContactUs"
+          href="/about"
           className="rounded-full w-[13.802vw] h-[2.656vw] bg-[#51B5DD] font-bold text-white flex items-center justify-center"
         >
           <h1 className="text-[1.2vw]">Contact Us</h1>
