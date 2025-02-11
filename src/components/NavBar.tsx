d149751c8993cdb2575965dbb1844184c5a9a60a
ryanlukito <ryanlukito13@gmail.com>
2025-02-11 23:58:50 +0700

    editing navbar and header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,42 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { IoPersonSharp } from "react-icons/io5";
+import { IoMdSettings } from "react-icons/io";
+import { RiLogoutBoxLine } from "react-icons/ri";
 
 const sources = [
   {
-    src_img: "/images/profile.png",
+    id: "profile",
+    src_img: <IoPersonSharp className="fill-current" />,
     text: "Profile",
-    classnames: "w-[1.111vw] h-[1.111vw]",
-    linkto: "/profile",
+    classnames: "text-[1.111vw]",
+    linkto: "/Profile",
   },
   {
-    src_img: "/images/tracker.png",
+    id: "tracker",
+    src_img: <FaArrowTrendUp className="fill-current" />,
     text: "Tracker",
-    classnames: "w-[1.432vw] h-[0.677vw]",
-    linkto: "/tracker",
+    classnames: "text-[1.432vw]",
+    linkto: "/Tracker",
   },
   {
-    src_img: "/images/profile.png",
+    id: "settings",
+    src_img: <IoMdSettings className="fill-current" />,
     text: "Settings",
-    classnames: "w-[1.111vw] h-[1.111vw]",
-    linkto: "/settings",
+    classnames: "text-[1.719vw]",
+    linkto: "/SettingsPage",
   },
 ];
 
-const NavBar = () => {
+const NavBar: React.FC = () => {
+  const [active, setActive] = useState<string | null>(null);
+
   return (
-    <div className="w-[8.021vw] h-screen bg-[#F9FAFC] rounded-r-[0.781vw] flex flex-col items-center justify-evenly text-[#000000] ">
+    <div className="w-[8.021vw] h-screen bg-[#F9FAFC] rounded-r-[0.781vw] flex flex-col items-center justify-evenly text-[#000000]">
       <Image
         src="/images/LOGO INSPEKTRA PLUS 4.png"
         alt="logo"
@@ -33,46 +44,58 @@ const NavBar = () => {
         height={10000}
         className="w-[4.948vw] h-[7.24vw]"
       />
-      {sources.map((item, index) => (
-        <div key={index} className="flex flex-col relative">
+      {sources.map((item) => (
+        <div key={item.id} className="flex flex-col relative group">
           <Link
             href={item.linkto}
-            className="0.625vw flex flex-col items-center justify-center"
+            className="flex flex-col items-center justify-center"
+            onClick={() => setActive(item.id)}
           >
-            <div
-              className=" p-[1vw] hover:before:content-[''] hover:before:absolute hover:before:w-[3vw] hover:before:h-[3vw] 
-                    hover:before:bg-orange-500 hover:before:rounded-full hover:before:opacity-50 hover:before:z-10 hover:before:top-0
-                    hover:before:left-1/2 hover:before:-translate-x-1/2"
-            >
-              <Image
-                src={item.src_img}
-                alt="logo"
-                width={10000}
-                height={10000}
-                className={item.classnames}
-              />
+            <div className="p-[1vw] relative">
+              <div
+                className={`absolute w-[3vw] h-[3vw] bg-[#FE8021] rounded-full ${
+                  active === item.id
+                    ? "opacity-100"
+                    : "opacity-0 group-hover:opacity-100"
+                } z-0 
+                top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 flex items-center justify-center`}
+              >
+                <div className={`${item.classnames} text-white z-10`}>
+                  {item.src_img}
+                </div>
+              </div>
+              <div
+                className={`${item.classnames} ${
+                  active === item.id
+                    ? "text-white"
+                    : "text-[#FE8021] group-hover:text-white"
+                } z-10 transition-all duration-200 ease-in-out`}
+              >
+                {item.src_img}
+              </div>
             </div>
             <h1 className="text-[1vw]">{item.text}</h1>
           </Link>
         </div>
       ))}
 
-      <div className="flex flex-col items-center justify-center relative">
-        <div
-          className="p-[1vw] hover:before:content-[''] hover:before:absolute hover:before:w-[3vw] hover:before:h-[3vw] 
-                    hover:before:bg-orange-500 hover:before:rounded-full hover:before:opacity-50 hover:before:z-10 hover:before:top-0
-                    hover:before:left-1/2 hover:before:-translate-x-1/2"
-        >
-          <Image
-            src="/images/profile.png"
-            alt="logo"
-            width={10000}
-            height={10000}
-            className="w-[1.111vw] h-[1.111vw]"
-          />
+      <button className="flex flex-col items-center justify-center relative group">
+        <div className="p-[1vw] relative">
+          <div
+            className="absolute w-[3vw] h-[3vw] bg-[#FE8021] rounded-full opacity-0 group-hover:opacity-100 z-0 
+                          top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 flex items-center justify-center"
+          >
+            <div className="text-[1.658vw] text-white z-10">
+              <RiLogoutBoxLine className="fill-current" />
+            </div>
+          </div>
+
+          <div className="text-[1.658vw] text-[#FE8021] group-hover:text-white z-10 transition-all duration-200 ease-in-out">
+            <RiLogoutBoxLine className="fill-current" />
+          </div>
         </div>
-        <h1>Log Out</h1>
-      </div>
+        <h1 className="text-[1vw]">Log Out</h1>
+      </button>
     </div>
   );
 };
