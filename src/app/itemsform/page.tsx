2b800a78c0d37f26385324ca0e5005f563e0b288
ryanlukito <ryanlukito13@gmail.com>
2025-03-04 15:22:54 +0700

    dependencies

--- src/app/itemsform/page.tsx
+++ src/app/itemsform/page.tsx
@@ -1,14 +1,69 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Dropdown from "@/components/Dropdown";
 import { useSearchParams } from "next/navigation";
+import { createClient } from "@supabase/supabase-js";
+// import { table } from "console";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
 const ItemsForm = () => {
   const [selected, setSelected] = useState("Status Condition");
+  const [jenisSarana, setJenisSarana] = useState<string | null>(null);
+  // const [columnName, setColumnName] = useState<any[]>([]);
+
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
+
+  useEffect(() => {
+    const fetch_jenis_sarana = async () => {
+      if (!id) return;
+
+      const { data, error } = await supabase
+        .from("item")
+        .select("jenis_sarana")
+        .eq("id_item", id)
+        .single();
+
+      if (error) {
+        console.error("Error fetching item: ", error);
+      } else {
+        setJenisSarana(data?.jenis_sarana || "Unknown");
+      }
+    };
+
+    fetch_jenis_sarana();
+  }, [id]);
+
+  useEffect(() => {
+    if (!jenisSarana) return;
+
+    const table_name = `inspeksi_${jenisSarana}`;
+    // console.log(table_name);
+
+    const fetch_table_name = async () => {
+      const { data, error } = await supabase
+        .from(table_name)
+        .select("*")
+        .limit(1); // Fetch only 1 row to save bandwidth
+
+      if (error) {
+        console.error("Error fetching columns: ", error);
+      } else if (data && data.length > 0) {
+        const columns = Object.keys(data[0]);
+        console.log("Column Names: ", columns);
+      } else {
+        console.warn("Table is empty — no rows to extract columns from.");
+      }
+    };
+
+    fetch_table_name();
+  }, [jenisSarana]);
 
   return (
     <div className="w-screen h-screen bg-[#fcfcfc] flex flex-col items-center">
@@ -46,7 +101,9 @@ const ItemsForm = () => {
           ]}
         />
         <h1 className="text-[1.302vw] mr-[3.5vw]">Lokasi</h1>
-        <p>{id}</p>
+        <p>id item: {id}</p>
+        <p>Jenis Sarana: {jenisSarana}</p>
+        {/* <p>Nama Kolom: {JSON.stringify(columnName)}</p> */}
       </div>
     </div>
   );
