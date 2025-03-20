b03d984d5792cadc522d86d8514f8e6d854d8c10
Hafidh <hafidhhusna@mail.ugm.ac.id>
2025-03-20 21:37:56 +0700

    asemboh

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -6,8 +6,19 @@ const prisma = new PrismaClient();
 export async function GET() {
   try {
     const items = await prisma.item.findMany({
+      include: {
+        nama_lokasi : {select : {nama_lokasi: true}}
+      },
       orderBy: { id_item: "asc" },
     });
+
+    console.log(items)
+
+    // const result = items.map((item) => ({
+    //   id_item : item.id_item,
+    //   nama_item
+
+    // }))
 
     return NextResponse.json({ success: true, items });
   } catch (error) {
--- src/app/api/titik-lokasi/route.ts
+++ src/app/api/titik-lokasi/route.ts
@@ -9,18 +9,18 @@ export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
     const lokasi_id = searchParams.get("lokasi_id");
 
-    if (lokasi_id) {
-      // Jika lokasi_id diberikan, ambil titik_lokasi berdasarkan lokasi_id
-      const titikLokasi = await prisma.titik_lokasi.findMany({
-        where: { lokasi_id: Number(lokasi_id) },
-        select: {
-          id_titik_lokasi: true,
-          nama_titik_lokasi: true,
-        },
-      });
+    // if (lokasi_id) {
+    //   // Jika lokasi_id diberikan, ambil titik_lokasi berdasarkan lokasi_id
+    //   const titikLokasi = await prisma.titik_lokasi.findMany({
+    //     where: { lokasi_id: Number(lokasi_id) },
+    //     select: {
+    //       id_titik_lokasi: true,
+    //       nama_titik_lokasi: true,
+    //     },
+    //   });
 
-      return NextResponse.json({ tipe: "titik_lokasi", data: titikLokasi }, { status: 200 });
-    } else {
+    //   return NextResponse.json({ tipe: "titik_lokasi", data: titikLokasi }, { status: 200 });
+    // } else {
       // Jika tidak ada lokasi_id, ambil daftar lokasi
       const lokasiList = await prisma.lokasi.findMany({
         select: {
@@ -30,7 +30,7 @@ export async function GET(req: Request) {
       });
 
       return NextResponse.json({ tipe: "lokasi", data: lokasiList }, { status: 200 });
-    }
+    // }
   } catch (error) {
     return NextResponse.json(
       { error: "Terjadi kesalahan server", details: (error as Error).message },
