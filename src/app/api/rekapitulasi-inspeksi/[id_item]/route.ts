16b449075565442fc86b4bdd2a7daae79d720895
Hafidh <hafidhhusna@mail.ugm.ac.id>
2025-03-24 19:51:39 +0700

    fix api rekapitulasi inspeksi

--- src/app/api/rekapitulasi-inspeksi/[id_item]/route.ts
+++ src/app/api/rekapitulasi-inspeksi/[id_item]/route.ts
@@ -5,51 +5,54 @@ const prisma = new PrismaClient();
 
 export async function GET(
   req: Request,
-  { params }: { params: { id_item: string } }
+  context: { params: { id_item: string } }
 ) {
-  const id_item = parseInt(params.id_item);
+  const { id_item } = await context.params;
 
-  if (isNaN(id_item)) {
-    return NextResponse.json({ error: "ID item tidak valid" }, { status: 400 });
+  if (isNaN(Number(id_item))) {
+    return NextResponse.json(
+      { error: "ID item tidak valid" },
+      { status: 400 }
+    );
   }
 
   try {
     const inspeksiTables = {
       inspeksi_sprinkler: await prisma.inspeksi_sprinkler.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) }, // Convert to Number
       }),
       inspeksi_APAP: await prisma.inspeksi_APAP.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) },
       }),
       inspeksi_detector: await prisma.inspeksi_detector.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) },
       }),
       inspeksi_hidran_bangunan: await prisma.inspeksi_hidran_bangunan.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) },
       }),
       inspeksi_hidran_halaman: await prisma.inspeksi_hidran_halaman.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) },
       }),
       inspeksi_kotak_p3k: await prisma.inspeksi_kotak_p3k.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) },
       }),
       inspeksi_ruang_mns: await prisma.inspeksi_ruang_mns.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) },
       }),
       inspeksi_rumah_pompa_hidran:
         await prisma.inspeksi_rumah_pompa_hidran.findMany({
-          where: { id_item },
+          where: { id_item: Number(id_item) },
         }),
       inspeksi_sarana_jalan_keluar:
         await prisma.inspeksi_sarana_jalan_keluar.findMany({
-          where: { id_item },
+          where: { id_item: Number(id_item) },
         }),
       inspeksi_scba: await prisma.inspeksi_scba.findMany({
-        where: { id_item },
+        where: { id_item: Number(id_item) },
       }),
       inspeksi_spill_containment_room:
         await prisma.inspeksi_spill_containment_room.findMany({
-          where: { id_item },
+          where: { id_item: Number(id_item) },
         }),
     };
 
