bc6a909e6d0a2eebafbbf6486a95f7d723070f9d
Hafidh <hafidhhusna@mail.ugm.ac.id>
2025-04-19 20:15:14 +0700

    add : readiness based on month and year

--- src/app/api/rekapitulasi/route.ts
+++ src/app/api/rekapitulasi/route.ts
@@ -1,163 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import {prisma} from "@/lib/db"; 
-
-export async function GET() {
-  try {
-    const lokasiData = await prisma.lokasi.findMany({
-      include: {
-        item: {
-          include: {
-            inspeksi_sprinkler: { 
-              select: { 
-                kondisi_internal_pipa: true,
-                katup_kontrol: true,
-                koneksi_pemadam_kebakaran: true,
-                penyangga: true,
-                kepala_sprinkler: true,
-                sprinkler_cadangan: true,
-                perangkat_pengawasan_katup: true,
-                perangkat_alarm_aliran_air: true
-            } },
-
-            inspeksi_APAP: { 
-              select: { 
-                visibilitas: true ,
-                kemudahan_akses: true,
-                tekanan: true,
-                kepenuhan_isi: true,
-                segel_pengaman: true,
-                selang_dan_nozel: true,
-                abnormalitas_fisik: true
-              } },
-            inspeksi_detector: { 
-              select: { 
-                lokasi: true,
-                penghalang_detektor: true,
-                kerusakan_fisik: true,
-                audible_dan_visible: true,
-                lampu: true,
-                power_supply: true,
-                prosedur_manufaktur: true
-               } },
-            inspeksi_hidran_bangunan: { 
-              select: { 
-                tanda_informasi_desain_hidraulik: true,
-                koneksi_selang: true,
-                perpipaan: true,
-                selang: true,
-                nozel: true,
-                perangkat_penyimpan_selang: true,
-                kabinet_penyimpanan: true
-              } },
-            inspeksi_hidran_halaman: { 
-              select: { 
-                kemudahan_akses: true,
-                outlet_bagian_atas_hidran_pilar: true,
-                batang_pilar: true,
-                outlet: true,
-                nozel: true,
-                mur_pengoperasian: true,
-                ketersediaan_kunci_hidran: true
-               } },
-            inspeksi_kotak_p3k: { 
-              select: { 
-                perban: true,
-                plester: true,
-                kapas: true,
-                gunting_peniti_pinset: true,
-                sarung_tangan_masker: true,
-                gelas_kecil: true,
-                aquades: true,
-                povidon_iodin: true,
-                kantong_plastik: true,
-                buku_panduan_daftar_isi: true
-               } },
-            inspeksi_ruang_mns: { 
-              select: { 
-                antena: true,
-                transceiver: true,
-                backup: true,
-                ventilasi: true,
-                penyampai_visual: true,
-                monitoring_personel: true,
-                audibilitas: true,
-                peralatan_notifikasi: true,
-                aktivasi_notifikasi_massal: true,
-                penyediaan_alat: true,
-                kapabilitas_komunikasi: true,
-                daya_sekunder: true,
-                ketersediaan_alat_komunikasi: true,
-                pemantauan_alat_komunikasi: true
-               } },
-            inspeksi_rumah_pompa_hidran: { 
-              select: { 
-                pencahayaan_normal: true,
-                pencahayaan_darurat: true,
-                ventilasi: true,
-                sistem_drainase: true,
-                pagar_pelindung_penggerak: true,
-                sistem_pompa: true,
-                daya_listrik_pompa: true,
-                motor_penggerak: true,
-                baterai: true,
-                penyediaan_bahan_bakar: true,
-                buku_petunjuk_perkakas_khusus_sparepart: true
-              } },
-            inspeksi_sarana_jalan_keluar: { 
-              select: { 
-                eksit: true,
-                eksit_pelepasan: true,
-                iluminasi: true,
-                pencahayaan_darurat: true,
-                penandaan: true,
-                tangga_kebakaran: true
-               } },
-            inspeksi_scba: { 
-              select: { 
-                facepiece: true,
-                rangka_rakitan_harness: true,
-                silinder_tabung:true,
-                selang:true,
-                EOSTI:true,
-                regulator:true
-              } },
-            inspeksi_spill_containment_room: { 
-              select: { 
-                fasilitas: true,
-                sistem_keamanan: true,
-                sistem_pencegahan_kebakaran:true,
-                sistem_pencegahan_tumpahan_limbah:true,
-                sistem_penanggulangan_keadaan_darurat:true
-               } },
-          },
-        },
-      },
-    });
-    console.log("lokasi data : ", JSON.stringify(lokasiData, null, 2))
-    console.log("data lokasi dari prisma : ", lokasiData);
-
-    // Rekapitulasi berdasarkan lokasi
-    const rekapitulasi = lokasiData.map((lokasi) => {
-      const totalItems = lokasi.item.length;
-      console.log(lokasi.item.length)
-      const readyItems = lokasi.item.filter((item) => checkStatus(item) === "READY").length;
-      const persentaseReady = totalItems > 0 ? (readyItems / totalItems) * 100 : 0;
-
-      return {
-        lokasi: lokasi.nama_lokasi,
-        total_items: totalItems,
-        ready_items: readyItems,
-        persentase_ready: `${persentaseReady.toFixed(2)}%`,
-      };
-    });
-    console.log("rekapitulasi yang dikirim ke FE : ", rekapitulasi);
-
-    return NextResponse.json({ data: rekapitulasi });
-  } catch (error) {
-    console.error("Error fetching rekapitulasi lokasi:", error);
-    return NextResponse.json({ error: "Terjadi kesalahan" }, { status: 500 });
-  }
-}
 
 // Fungsi untuk menentukan status inspeksi tiap item
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -192,3 +34,210 @@ const checkStatus = (item: any) => {
 
   return "READY";
 };
+
+export async function GET(req : NextRequest) {
+  const {searchParams} = new URL(req.url);
+  const bulan = parseInt(searchParams.get("bulan") || "");
+  const tahun = parseInt(searchParams.get("tahun") || "");
+
+  if(!bulan || !tahun){
+    console.log("Bulan dan tahun wajib diisi")
+    return NextResponse.json({error: "Bulan dan tahun wajib diisi"}, {status: 400});
+  }
+
+  const awalBulan = new Date(tahun, bulan - 1, 1);
+  const akhirBulan = new Date(tahun, bulan, 0, 23, 59, 59);
+
+  try {
+    const lokasiData = await prisma.lokasi.findMany({
+      include: {
+        item: {
+          where: {
+            OR: [
+              { inspeksi_sprinkler: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_APAP: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_detector: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_hidran_bangunan: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_hidran_halaman: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_kotak_p3k: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_ruang_mns: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_rumah_pompa_hidran: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_sarana_jalan_keluar: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_scba: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+              { inspeksi_spill_containment_room: { some: { createdAt: { gte: awalBulan, lte: akhirBulan } } } },
+            ]
+          },
+          include: {
+            inspeksi_sprinkler: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                kondisi_internal_pipa: true,
+                katup_kontrol: true,
+                koneksi_pemadam_kebakaran: true,
+                penyangga: true,
+                kepala_sprinkler: true,
+                sprinkler_cadangan: true,
+                perangkat_pengawasan_katup: true,
+                perangkat_alarm_aliran_air: true,
+              }
+            },
+            inspeksi_APAP: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                visibilitas: true,
+                kemudahan_akses: true,
+                tekanan: true,
+                kepenuhan_isi: true,
+                segel_pengaman: true,
+                selang_dan_nozel: true,
+                abnormalitas_fisik: true
+              }
+            },
+            inspeksi_detector: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                lokasi: true,
+                penghalang_detektor: true,
+                kerusakan_fisik: true,
+                audible_dan_visible: true,
+                lampu: true,
+                power_supply: true,
+                prosedur_manufaktur: true
+              }
+            },
+            inspeksi_hidran_bangunan: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                tanda_informasi_desain_hidraulik: true,
+                koneksi_selang: true,
+                perpipaan: true,
+                selang: true,
+                nozel: true,
+                perangkat_penyimpan_selang: true,
+                kabinet_penyimpanan: true
+              }
+            },
+            inspeksi_hidran_halaman: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                kemudahan_akses: true,
+                outlet_bagian_atas_hidran_pilar: true,
+                batang_pilar: true,
+                outlet: true,
+                nozel: true,
+                mur_pengoperasian: true,
+                ketersediaan_kunci_hidran: true
+              }
+            },
+            inspeksi_kotak_p3k: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                perban: true,
+                plester: true,
+                kapas: true,
+                gunting_peniti_pinset: true,
+                sarung_tangan_masker: true,
+                gelas_kecil: true,
+                aquades: true,
+                povidon_iodin: true,
+                kantong_plastik: true,
+                buku_panduan_daftar_isi: true
+              }
+            },
+            inspeksi_ruang_mns: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                antena: true,
+                transceiver: true,
+                backup: true,
+                ventilasi: true,
+                penyampai_visual: true,
+                monitoring_personel: true,
+                audibilitas: true,
+                peralatan_notifikasi: true,
+                aktivasi_notifikasi_massal: true,
+                penyediaan_alat: true,
+                kapabilitas_komunikasi: true,
+                daya_sekunder: true,
+                ketersediaan_alat_komunikasi: true,
+                pemantauan_alat_komunikasi: true
+              }
+            },
+            inspeksi_rumah_pompa_hidran: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                pencahayaan_normal: true,
+                pencahayaan_darurat: true,
+                ventilasi: true,
+                sistem_drainase: true,
+                pagar_pelindung_penggerak: true,
+                sistem_pompa: true,
+                daya_listrik_pompa: true,
+                motor_penggerak: true,
+                baterai: true,
+                penyediaan_bahan_bakar: true,
+                buku_petunjuk_perkakas_khusus_sparepart: true
+              }
+            },
+            inspeksi_sarana_jalan_keluar: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                eksit: true,
+                eksit_pelepasan: true,
+                iluminasi: true,
+                pencahayaan_darurat: true,
+                penandaan: true,
+                tangga_kebakaran: true
+              }
+            },
+            inspeksi_scba: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                facepiece: true,
+                rangka_rakitan_harness: true,
+                silinder_tabung: true,
+                selang: true,
+                EOSTI: true,
+                regulator: true
+              }
+            },
+            inspeksi_spill_containment_room: {
+              where: { createdAt: { gte: awalBulan, lte: akhirBulan } },
+              select: {
+                fasilitas: true,
+                sistem_keamanan: true,
+                sistem_pencegahan_kebakaran: true,
+                sistem_pencegahan_tumpahan_limbah: true,
+                sistem_penanggulangan_keadaan_darurat: true
+              }
+            },
+          }
+        }
+      }
+    });
+    
+    console.log("lokasi data : ", JSON.stringify(lokasiData, null, 2))
+    console.log("data lokasi dari prisma : ", lokasiData);
+
+    // Rekapitulasi berdasarkan lokasi
+    const rekapitulasi = lokasiData.map((lokasi) => {
+      const totalItems = lokasi.item.length;
+      console.log(lokasi.item.length)
+      const readyItems = lokasi.item.filter((item) => checkStatus(item) === "READY").length;
+      const persentaseReady = totalItems > 0 ? (readyItems / totalItems) * 100 : 0;
+
+      return {
+        lokasi: lokasi.nama_lokasi,
+        total_items: totalItems,
+        ready_items: readyItems,
+        persentase_ready: `${persentaseReady.toFixed(2)}%`,
+      };
+    });
+    console.log("rekapitulasi yang dikirim ke FE : ", rekapitulasi);
+
+    return NextResponse.json({ data: rekapitulasi });
+  } catch (error) {
+    console.error("Error fetching rekapitulasi lokasi:", error);
+    return NextResponse.json({ error: "Terjadi kesalahan" }, { status: 500 });
+  }
+}
